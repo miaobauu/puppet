1ee64b79e2964e7988346f1866de379297ad633d
miaobau <41647367+miaobau@users.noreply.github.com>
2018-07-24 22:07:54 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,3 +8,9 @@ node default {
 node 'ip-172-31-44-1.eu-west-1.compute.internal' {
   include role::master_server
 }
+node /^web/ { 
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
+}
