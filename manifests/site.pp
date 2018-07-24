e427a1362f5f05bc7920b69d66ca0edd44e8671a
miaobau <41647367+miaobau@users.noreply.github.com>
2018-07-24 22:01:34 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,10 @@
 node default {
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
+}
+node 'ip-172-31-44-1.eu-west-1.compute.internal' {
+  include role::master_server
 }
