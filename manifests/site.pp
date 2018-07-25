c500a42428c2bc75c530af60d68df5ec9f1266c0
miaobau <41647367+miaobau@users.noreply.github.com>
2018-07-25 11:30:17 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
 node default {
+package { 'openssh':
+  ensure  => installed,
+  name    => $ssh,
+  require => Package['openssl'],
+}
 }
 node 'ip-172-31-44-1.eu-west-1.compute.internal' {
   include role::master_server
